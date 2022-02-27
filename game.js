3ac9c399bd87b6ddbe8e37cd136f00b8ce15c848
Stacey Csikos <stacey@Staceys-MBP.lan>
2022-02-26 21:01:34 -0500

    arranged cards better

--- game.js
+++ game.js
@@ -8,30 +8,26 @@ const spades = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
  
 let cards = clubs.concat(diamonds, hearts, spades)
 
+function shuffle(array) {
+  let copy = [], n = array.length, i;
+
+  // While there remain elements to shuffle…
+  while (n) {
+
+    // Pick a remaining element…
+    i = Math.floor(Math.random() * array.length);
+
+    // If not already shuffled, move it to the new array.
+    if (i in array) {
+      copy.push(array[i]);
+      delete array[i];
+      n--;
+    }
+  }
+
+  return copy;
+}
 
 
-// function shuffle(array) {
-//   let copy = [], n = array.length, i;
-
-//   // While there remain elements to shuffle…
-//   while (n) {
-
-//     // Pick a remaining element…
-//     i = Math.floor(Math.random() * array.length);
-
-//     // If not already shuffled, move it to the new array.
-//     if (i in array) {
-//       copy.push(array[i]);
-//       delete array[i];
-//       n--;
-//     }
-//   }
-
-//   return copy;
-// }
-
-
-// shuffle(combinecards)
-
-// console.log(shuffle(combinecards))
+console.log(shuffle(combinecards))
 
