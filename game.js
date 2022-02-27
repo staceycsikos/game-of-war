c6ec4416e7b995b8343a941184076c28dbee8ca8
Stacey Csikos <stacey@Staceys-MBP.lan>
2022-02-26 21:18:13 -0500

    created two players with split deck

--- game.js
+++ game.js
@@ -9,25 +9,32 @@ const spades = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
 let cards = clubs.concat(diamonds, hearts, spades)
 
 function shuffle(array) {
-  let copy = [], n = array.length, i;
+  var m = array.length, t, i;
 
   // While there remain elements to shuffle…
-  while (n) {
+  while (m) {
 
     // Pick a remaining element…
-    i = Math.floor(Math.random() * array.length);
+    i = Math.floor(Math.random() * m--);
 
-    // If not already shuffled, move it to the new array.
-    if (i in array) {
-      copy.push(array[i]);
-      delete array[i];
-      n--;
-    }
+    // And swap it with the current element.
+    t = array[m];
+    array[m] = array[i];
+    array[i] = t;
   }
 
-  return copy;
+  return array;
 }
 
 
-console.log(shuffle(combinecards))
+
+console.log(shuffle(cards))
+
+const splitDeck = Math.ceil(cards.length / 2)
+
+let player1 = cards.splice(0, splitDeck)
+let player2 = cards.splice(-splitDeck)
+
+console.log(player1)
+console.log(player2)
 
