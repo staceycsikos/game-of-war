52be33ad512e2f495f54d79a96d91e08e8370e82
Stacey Csikos <stacey@Staceys-MacBook-Pro.local>
2022-03-02 12:23:36 -0500

    Mustafa's help

--- game2.js
+++ game2.js
@@ -1,42 +1,90 @@
 class Cards {
   constructor(suit, rank, score) {
-    this.suit = suit
-    this.rank = rank
-    this.score = score
+    this.suit = suit;
+    this.rank = rank;
+    this.score = score;
   }
 }
 
-class Deck{
+class Deck {
   constructor() {
-    this.cards = []  
-    this.createDeck()
+    this.cards = [];
+    this.createDeck();
   }
   createDeck() {
-    let suits = ["Hearts", "Spades", "Clubs", "Diamonds"]
-    let rank = ["Ace", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"]
+    let suits = ["Hearts", "Spades", "Clubs", "Diamonds"];
+    let rank = [
+      "Ace",
+      "1",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "Jack",
+      "Queen",
+      "King",
+    ];
 
-    for (let i = 0; i < suits.length; i++){
-      for (let j = 0; j < rank.length; j++){
-        this.cards.push(new Cards(suits[i], rank[j], j+1))
+    for (let i = 0; i < suits.length; i++) {
+      for (let j = 0; j < rank.length; j++) {
+        this.cards.push(new Cards(suits[i], rank[j], j + 1));
       }
     }
-   
+    this.shuffle();
   }
-  draw(){
-    return this.cards.pop()
+  shuffle() {
+    let m = this.cards.length,
+      t,
+      i;
+    while (m) {
+      i = Math.floor(Math.random() * m--);
+      t = this.cards[m];
+      this.cards[m] = this.cards[i];
+      this.cards[i] = t;
+    }
+    return this.cards;
+  }
+
+  draw() {
+    return this.cards.pop();
   }
 }
+let createDeck = (deck = []) => {
+  let suits = ["Hearts", "Spades", "Clubs", "Diamonds"];
+  let rank = [
+    "Ace",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "Jack",
+    "Queen",
+    "King",
+  ];
+
+  for (let i = 0; i < suits.length; i++) {
+    for (let j = 0; j < rank.length; j++) {
+      deck.push(new Cards(suits[i], rank[j], j + 1));
+    }
+  }
+  return deck;
+};
+let beeBrand = new Deck();
+
+let player1 = beeBrand.cards.slice(0, 26);
+let player2 = beeBrand.cards.slice(26, 52);
 
 
-let newDeck = new Deck()
-console.log(newDeck.cards)
-
-
-
-console.log(newDeck.cards.length)
-console.log(newDeck.draw())
-console.log(newDeck.draw())
-console.log(newDeck.draw())
-console.log(newDeck.draw())
-console.log(newDeck.cards.length)
-
+console.log(player1.length, player2.length);
+console.log(createDeck());
