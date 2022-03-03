63e46177d668c9fce34fab0d5158c171763df038
Stacey Csikos <stacey@Staceys-MacBook-Pro.local>
2022-03-03 16:53:26 -0500

    oouf

--- newgame.js
+++ newgame.js
@@ -10,6 +10,14 @@ class Player {
     }
     this.lastPlayed = this.cardPlay[this.cardPlay.length - 1];
   }
+  takeHand(table) {
+    for (let i = 0; i < table.length; i++) {
+      this.deck.push(table[i]);
+    }
+  }
+  clear() {
+    this.cardPlay.splice(0, this.cardPlay.length);
+  }
 }
 
 class Table {
@@ -20,6 +28,9 @@ class Table {
     for (let i = 0; i < deck.length; i++) {
       this.deck.push(deck[i]);
     }
+  }
+  clear() {
+    this.deck.splice(0, this.deck.length);
   }
 }
 
@@ -50,11 +61,18 @@ let shuffle = (deck) => {
   return deck;
 };
 
+let clearAll = (p1, p2, bigT) => {
+  p1.clear();
+  p2.clear();
+  bigT.clear();
+};
+
 let playRound = () => {
   playerOne.draw(1);
   playerTwo.draw(1);
   table.add(playerOne.cardPlay);
   table.add(playerTwo.cardPlay);
+
   console.log(table.deck);
   console.log("\n");
 
@@ -66,30 +84,59 @@ let playRound = () => {
   );
   if (playerOne.lastPlayed.value > playerTwo.lastPlayed.value) {
     console.log("player one won ");
+    playerOne.takeHand(table.deck);
+    clearAll(playerOne, playerTwo, table);
   } else if (playerOne.lastPlayed.value < playerTwo.lastPlayed.value) {
     console.log("player two won");
+    playerTwo.takeHand(table.deck);
+    clearAll(playerOne, playerTwo, table);
   } else if (playerOne.lastPlayed.value == playerTwo.lastPlayed.value) {
     console.log("players tied");
-  }
+    playerOne.draw(4);
+    playerTwo.draw(4);
+    table.add(playerOne.cardPlay);
+    table.add(playerTwo.cardPlay);
 
+    gameOver = true;
+  }
+  console.log("\n");
+  console.log(playerOne.deck.length, playerTwo.deck.length);
 };
 
 let runGame = () => {
   while (!gameOver) {
-    if (playerOne.deck.length == 0 ||)
+    if (playerOne.deck.length == 0 || playerTwo.deck.length == 0) {
+      gameOver = true;
+    } else {
+      playRound();
+    }
+    gameOver = true;
   }
-}
-
-
-
-
+  console.log(
+    "player lengths:   ",
+    playerOne.deck.length,
+    "   ",
+    playerTwo.deck.length
+  );
+};
+let war = () => {
+  console.log("players tied");
+  table.add.push(playerOne.deck.slice(0, 2));
+  table.add.push(playerTwo.deck.slice(0, 2));
+  playerOne.draw(1);
+  playerTwo.draw(1);
+  table.add(playerOne.cardPlay);
+  table.add(playerTwo.cardPlay);
+};
+let gameOver = false;
 let deck = createDeck();
 deck = shuffle(deck);
 const playerOne = new Player(deck.slice(0, 26));
 const playerTwo = new Player(deck.slice(26, 52));
 const table = new Table();
-let gameOver = false;
+// let gameOver = false;
 
+runGame();
 
 // console.log(playerOne.deck.length, playerTwo.deck.length);
 
