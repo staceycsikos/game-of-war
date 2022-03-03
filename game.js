c39e75fa20f346e9c1faa30564bc4e79e71063c6
Stacey Csikos <stacey@staceys-mbp.myfiosgateway.com>
2022-03-03 04:47:01 -0500

    combined concepts from g1 g2

--- game.js
+++ game.js
@@ -30,13 +30,61 @@ shuffle(cards);
 let player1 = cards.slice(0, 26);
 let player2 = cards.slice(26, 52);
 
-console.log(player1);
-console.log(player2);
+// console.log(player1);
+// console.log(player2);
 
-const start = "Game Starting NOW!";
+console.log("Game Starting NOW!");
 
-console.log(start);
-
-// function play = {
-
+// function draw(flip) {
+//   return flip.pop()
 // }
+
+let x = 2 /*player1.pop()*/
+let y = 2 /*player2.pop()*/
+let z = []
+let a = []
+
+console.log (x, y)
+
+// console.log(player1.length, player2.length)
+
+if (x > y) {
+  console.log("Player 1 draws a " + x + " and wins!")
+} else if (y > x) {
+  console.log("Player 2 draws a " + y + " and wins!")
+} else if (y = x) {
+  z.push(player1.splice(0, 2))
+  a.push(player2.splice(0, 2))
+  console.log(z, a)
+  // war(a, z)
+  console.log("Both players play " + x + " WE ARE NOW ENTERING WARRRRR!!!!!")
+}
+
+// if (y = x) {
+//   z.push(player1.splice(0, 2))
+//   a.push(player2.splice(0, 2))
+//   // console.log(z, a)
+//   // war(a, z)
+//   console.log("WE ARE NOW ENTERING WARRRRR!!!!!")
+// }
+// else if (x > y) {
+//   console.log("Player 1 draws a " + x + " and wins!")
+// } else if (y > x) {
+//   console.log("Player 2 draws a " + y + " and wins!")
+// } 
+// console.log(x, y,)
+// console.log(z, a)
+
+// function war(a,z) {
+//   return "hi"
+// }
+// console.log(war(a, z))
+
+// if (console.log("Player 1 plays a "+ draw(player1)) > console.log("Player 2 plays a "+draw(player2))) {
+//     console.log("player 1 won") 
+// } else if (console.log("Player 1 plays a " + draw(player1)) > console.log("Player 2 plays a " + draw(player2))){
+//   console.log("player 2 won")}
+//  else {
+//   console.log("It's a tie")
+// }
+
